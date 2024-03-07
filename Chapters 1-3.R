df5177ab853ee59c7ffa893e23a0a3c6de174293
kndunlap <kylendunlap19@gmail.com>
2024-03-06 18:10:34 -0700

    yyaaaaa

--- Chapters 1-3.R
+++ Chapters 1-3.R
@@ -1,2 +1,52 @@
 print("Hello world!")
 print("eep")
+
+# Chapter 3 - Modeling Review ---------------------------------------------
+
+
+# 3.1 - An Example --------------------------------------------------------
+
+library(tidyverse)
+data(crickets, package = "modeldata")
+names(crickets)
+
+ggplot(crickets,
+       aes(x = temp, y = rate, color = species, pch = species, lty = species)) +
+  geom_point(size = 2) + 
+  geom_smooth(method = lm, se = FALSE, alpha = 0.5) +
+  scale_color_brewer(palette = "Paired") +
+  labs(x = "Temperature (C)", y = "Chirp Rate (per minute)")
+
+interaction_fit <- lm(rate ~ (temp + species)^2, data = crickets )
+interaction_fit
+
+par(mfrow = c(1,2))
+plot(interaction_fit, which = 1)
+plot(interaction_fit, which = 2)
+
+main_effect_fit <- lm(rate ~ temp + species, data = crickets)
+
+anova(main_effect_fit, interaction_fit)
+
+summary(main_effect_fit)
+
+new_values <- data.frame(species = "O. exclamationis", temp = 15:20)
+predict(main_effect_fit, new_values)
+
+
+# 3.4- Combining Base R Models and the Tidyverse ----------------------------
+
+split_by_species <-
+  crickets |>
+  group_nest(species)
+
+split_by_species
+
+model_by_species <-
+  split_by_species |>
+  mutate(model = map(data, ~ lm(rate ~ temp, data = .x)))
+
+model_by_species |>
+  mutate(coef = map(model, tidy)) |>
+  select(species, coef) |>
+  unnest(cols = c(coef))
---
+++ Chapters 4-9.R
@@ -0,0 +1,168 @@
+
+# Chapter 4 - The Ames Housing Data ---------------------------------------------------------------
+
+library(modeldata)
+data(ames)
+data(ames, package = "modeldata")
+
+dim(ames)
+
+# 4.1 - Exploring Features of Homes in Ames -------------------------------
+
+library(tidymodels)
+tidymodels_prefer()
+
+ggplot(ames, aes(x = Sale_Price)) +
+  geom_histogram(bins = 50, col = "white") +
+  scale_x_log10()
+
+ames <- ames |>
+  mutate(Sale_Price = log10(Sale_Price))
+
+
+# Chapter 5 - Spending our Data -------------------------------------------
+
+
+# 5.1 - Common Methods for Splitting Data ---------------------------------
+
+set.seed(501)
+ames_split <- initial_split(ames, prop = 0.80)
+ames_split
+
+ames_train <- training(ames_split)
+ames_test <- testing(ames_split)
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test <- testing(ames_split)
+
+
+# 5.2 - What about a validation set? --------------------------------------
+
+set.seed(52)
+ames_val_split <- initial_validation_split(ames, prop = c(0.6, 0.2))
+ames_val_split
+
+ames_train <- training(ames_val_split)
+ames_test <- testing(ames_val_split)
+ames_val <- validation(ames_val_split)
+
+
+library(tidymodels)
+data(ames)
+ames <- ames %>% mutate(Sale_Price = log10(Sale_Price))
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+
+# Chapter 6 - Fitting Models with parsnip ---------------------------------
+
+
+# 6.1 - Create a Model ----------------------------------------------------
+
+lm_model <-
+  linear_reg() |>
+  set_engine("lm")
+
+lm_form_fit <-
+  lm_model |>
+  fit(Sale_Price ~ Longitude + Latitude, data = ames_train)
+
+lm_xy_fit <- 
+  lm_model |>
+  fit_xy(
+    x = ames_train |> select(Longitude, Latitude),
+    y = ames_train |> pull(Sale_Price)
+  )
+
+lm_form_fit
+lm_xy_fit
+
+
+# 6.2 - Use the Model Results ---------------------------------------------
+
+lm_form_fit |> extract_fit_engine()
+
+
+# 6.3 - Make Predictions --------------------------------------------------
+
+ames_test_small <- ames_test |>
+  slice(1:5) 
+predict(lm_form_fit, new_data = ames_test_small)
+  
+ames_test_small |>
+  select(Sale_Price) |>
+  bind_cols(predict(lm_form_fit, ames_test_small)) |>
+  bind_cols(predict(lm_form_fit, ames_test_small, type = "pred_int"))
+
+tree_model <- 
+  decision_tree(min_n = 2) %>% 
+  set_engine("rpart") %>% 
+  set_mode("regression")
+
+tree_fit <- 
+  tree_model %>% 
+  fit(Sale_Price ~ Longitude + Latitude, data = ames_train)
+
+ames_test_small %>% 
+  select(Sale_Price) %>% 
+  bind_cols(predict(tree_fit, ames_test_small))
+
+
+library(tidymodels)
+data(ames)
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+lm_model <- linear_reg() %>% set_engine("lm")
+
+
+# Chapter 7 - A Model Workflow --------------------------------------------
+
+
+# 7.2 - Workflow Basics ---------------------------------------------------
+
+library(tidymodels)
+tidymodels_prefer()
+
+lm_model <-
+  linear_reg() |>
+  set_engine("lm")
+
+lm_wflow <- 
+  workflow() |>
+  add_model(lm_model)
+
+lm_wflow <-
+  lm_wflow |>
+  add_formula(Sale_Price ~ Longitude + Latitude)
+
+lm_fit <- fit(lm_wflow, ames_train)
+lm_fit
+
+predict(lm_fit, ames_test |> slice(1:3))
+
+lm_fit |> update_formula(Sale_Price ~ Longitude)
+
+
+# 7.3 - Adding Raw Variables to the workflow ------------------------------
+
+lm_wflow <-
+  lm_wflow |>
+  remove_formula() |>
+  add_variables(outcome = Sale_Price, predictors = c(Longitude, Latitude))
+
+lm_wflow
+
+
+# 7.4 - How Does a workflow() use the formula? ----------------------------
+
+
